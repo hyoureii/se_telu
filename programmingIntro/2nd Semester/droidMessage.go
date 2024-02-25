9d38acdd11083fad59cc7d3f1243d2b2de32b85a
hyoureii <hyoureibynaufal@gmail.com>
2024-02-26 05:38:11 +0700

    used Printf instead of Print

--- programmingIntro/2nd Semester/droidMessage.go
+++ programmingIntro/2nd Semester/droidMessage.go
@@ -10,9 +10,9 @@ func main() {
 	for i := 1; i <= n; i++ {
 		for j := 1; j <= n; j++ {
 			if i == j {
-				fmt.Print(" ")
+				fmt.Printf(" ")
 			} else {
-				fmt.Print("*")
+				fmt.Printf("*")
 			}
 		}
 		fmt.Printf("\n")
